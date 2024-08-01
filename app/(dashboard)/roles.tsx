90d99d09bc5e5373632b8f0622b0f85559f89799
Juan <juanpancalaveriano@gmail.com>
2024-08-01 10:49:05 -0500

    se agrego la ruta roles

---
+++ app/(dashboard)/roles.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function RolesPage() {
+  return (
+      <View>
+        <Text className="text-white">Roles</Text>
+    </View>
+      
+    
+  );
+}
